e4e68313db39aecb72fcd09d409596287988cf44
Jens Krause <email@jkrause.io>
2017-11-05 20:26:12 +0100

    Add `navbar` and `containers` (wip)

---
+++ src/Bulma/Components/NavBar.purs
@@ -0,0 +1,12 @@
+module Bulma.Components.NavBar
+  ( navbar
+  , navbarItem
+  ) where
+
+import Bulma.Core (ClazzName(..))
+
+navbar :: ClazzName
+navbar = ClazzName "navbar"
+
+navbarItem :: ClazzName
+navbarItem = ClazzName "navbar-item"
---
+++ src/Bulma/Layout/Layout.purs
@@ -0,0 +1,12 @@
+module Bulma.Layout.Layout
+  ( container
+  , footer
+  ) where
+
+import Bulma.Core (ClazzName(..))
+
+container :: ClazzName
+container = ClazzName "container"
+
+footer :: ClazzName
+footer = ClazzName "footer"
